72a1dbe505d7ae6470afafe2ffec7798aa1e2f72
PRTS <873265236@qq.com>
2025-03-31 11:51:41 +0800

    + if piglin was named then cancel transform event

--- src/main/java/com/eseabsolute/apzfolia/AntiPiglinZombification.java
+++ src/main/java/com/eseabsolute/apzfolia/AntiPiglinZombification.java
@@ -2,21 +2,19 @@ package com.eseabsolute.apzfolia;
 
 import com.eseabsolute.apzfolia.listeners.PiglinListener;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
 public final class AntiPiglinZombification extends JavaPlugin {
-    Logger logger;
-    PluginManager pluginManager;
+    public static Logger logger;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         logger = Bukkit.getLogger();
-        pluginManager = Bukkit.getPluginManager();
+        PluginManager pluginManager = Bukkit.getPluginManager();
         pluginManager.registerEvents(new PiglinListener(), this);
     }
 
--- src/main/java/com/eseabsolute/apzfolia/listeners/PiglinListener.java
+++ src/main/java/com/eseabsolute/apzfolia/listeners/PiglinListener.java
@@ -1,21 +1,38 @@
 package com.eseabsolute.apzfolia.listeners;
 
+import com.eseabsolute.apzfolia.AntiPiglinZombification;
 import io.papermc.paper.event.player.PlayerNameEntityEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.PiglinAbstract;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityTransformEvent;
 
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PiglinListener implements Listener {
+    Logger logger;
+
+    public PiglinListener() {
+        this.logger = AntiPiglinZombification.logger;
+    }
+
     @EventHandler
     public void onPlayerNameEntity(PlayerNameEntityEvent event) {
         if (event.getEntity().getType() == EntityType.PIGLIN) {
             PiglinAbstract piglin = (PiglinAbstract) event.getEntity();
             piglin.setImmuneToZombification(true);
-            Bukkit.getLogger().log(Level.INFO, "[APZFolia] Piglin with uuid " + piglin.getUniqueId() + "was named! Zombification disabled.");
+            logger.log(Level.INFO, "[APZFolia] Piglin with uuid " + piglin.getUniqueId() + "was named! Zombification disabled.");
+        }
+    }
+    @EventHandler
+    public void onEntityTransform(EntityTransformEvent event) {
+        if (event.getEntity().getType() == EntityType.PIGLIN) {
+            PiglinAbstract piglin = (PiglinAbstract) event.getEntity();
+            piglin.setImmuneToZombification(true);
+            logger.log(Level.INFO, "[APZFolia] Piglin with uuid " + piglin.getUniqueId() + "was named! Zombification disabled.");
+            event.setCancelled(true);
         }
     }
 }
